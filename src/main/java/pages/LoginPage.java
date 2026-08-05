1b05b423684ad997ebeea24b7c6f42e695fcd801
sergey-fishman <fishman.ser@gmail.com>
2026-08-05 21:03:48 +0300

    start project

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,35 @@
 package pages;
 
-public class LoginPage {
+import dto.UserLombok;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements
+                (new AjaxElementLocatorFactory
+                    (driver,10), this);
+    }
+
+    @FindBy(xpath = "//form/input[1]")
+    WebElement inputEmail;
+    @FindBy(css = "input[placeholder='Password']")
+    WebElement inputPassword;
+    @FindBy(xpath = "//button[@type='submit' and @name='registration']")
+    WebElement btnRegistration;
+    @FindBy(xpath = "//button[text()='Login']")
+    WebElement btnLogin;
+
+    public void typeLoginRegistrationForm(UserLombok user){
+        inputEmail.sendKeys(user.getUsername());
+        inputPassword.sendKeys(user.getPassword());
+    }
+
+    public void clickBtnRegistration(){
+        btnRegistration.click();
+    }
 }
--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,4 +1,30 @@
 package ui_tests;
 
-public class RegistrationTests {
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends AppManager {
+
+    @BeforeMethod
+    public void goToRegistrationLoginPage(){
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+
+    @Test
+    public void registrationPositiveTest(){
+        int i = new Random().nextInt(1000);
+        UserLombok user = UserLombok.builder()
+                .username("ser.gey"+i+"@ya.ru")
+                .password("Qwerty1234!")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+    }
 }

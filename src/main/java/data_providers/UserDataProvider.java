1c875ec9adc7ac5f4fe1413c607e71f5e058de26
sergey-fishman <fishman.ser@gmail.com>
2026-08-16 20:54:03 +0300

    wrong_email_password_cvs

---
+++ src/main/java/data_providers/UserDataProvider.java
@@ -0,0 +1,32 @@
+package data_providers;
+
+import dto.UserLombok;
+import org.testng.annotations.DataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class UserDataProvider {
+    @DataProvider
+    public Iterator<UserLombok> dataProviderWrongPasswordOrEmail() {
+        List<UserLombok> list = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader
+                ("src/test/resources/wrong_email_password.csv"))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] splitLine = line.split(",");
+                list.add(UserLombok.builder()
+                        .username(splitLine[0])
+                        .password(splitLine[1])
+                        .build());
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("created exception");
+        }
+        return list.listIterator();
+    }
+}

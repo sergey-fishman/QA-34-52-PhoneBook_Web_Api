27026ca3132fc7f916347e8473a71bf12c9821c5
sergey-fishman <fishman.ser@gmail.com>
2026-08-24 10:54:22 +0300

    added Logger

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 public abstract class BasePage {
     static WebDriver driver;
+    public Logger logger = LoggerFactory.getLogger(BasePage.class);
 
     public void setDriver(WebDriver wd) {
         driver = wd;
